9951078d27066ba8d0c7d6744e70e814575afdee
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-17 13:46:57 +0300

    Create main.vb

---
+++ main.vb
@@ -0,0 +1,10 @@
+Module Program
+    Sub Lulz(Optional i As String = "")
+        Console.Write("какой ты RYTP хочеш? ")
+        Dim j As String = Console.ReadLine()
+        Console.WriteLine(j+i)
+    End Sub
+    Sub Main()
+        Lulz(" RYTP")
+    End Sub
+End Module
