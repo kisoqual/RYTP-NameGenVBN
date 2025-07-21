a6477c70596d9fa130df2793a2d793057671ede0
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-21 21:52:27 +0300

    Update main.vb

--- main.vb
+++ main.vb
@@ -1,8 +1,16 @@
+Imports System '* говорят по хорошему лучше его юзать
+'Imports System.Console '* вроде тоже доступно с .NET 6
+
 Module Program
     Sub Lulz(Optional i As String = "")
         Console.Write("какой ты RYTP хочеш? ")
         Dim j As String = Console.ReadLine()
         Console.WriteLine(j+i)
+
+        '' так тоже можно, если подключен Imports System.Console
+        ' Write("какой ты RYTP хочеш? ")
+        ' Dim j As String = ReadLine()
+        ' WriteLine(j+i)
     End Sub
     Sub Main()
         Lulz(" RYTP")
